d51716a2b624387f1c56a38370037d98a81c86a2
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-10-18 22:12:07 +0300

    add def config()

--- config.py
+++ config.py
@@ -1,2 +1,22 @@
+from configparser import ConfigParser
+
+
+def config(filename="database.ini", section="postgresql") -> dict:
+    """
+    Производит парсинг данных из файла database.ini
+    """
+    parser = ConfigParser()
+    parser.read(filename)
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception(
+            'Section {0} is not found in the {1} file.'.format(section, filename))
+    return db
+
+
 COMPANIES = ['ООО Томору', 'Skyeng', 'ООО Аптрейд', 'ООО Фабрика Решений', 'Hexlet', 'ООО АЙТИ.СПЕЙС', 'Газпромбанк',
              'Bell Integrator', 'ООО Кью Лаб', 'Сбер. IT']  # список из 10 компаний
