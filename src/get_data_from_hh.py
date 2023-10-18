f09437cdf182221f20461d18dc92634d8a5ab9e9
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-10-18 19:50:28 +0300

    add def get_vacancies_from_company

--- src/get_data_from_hh.py
+++ src/get_data_from_hh.py
@@ -11,3 +11,12 @@ def get_company(keyword: str) -> dict:
     responce = requests.get('https://api.hh.ru/employers', params=params)
     company = responce.json()
     return company
+
+
+def get_vacancies_from_company(company: str) -> list:
+    """
+    Возвращает список вакансий компании
+    """
+    responce = requests.get(company['items'][0]['vacancies_url'])
+    vacancies = responce.json()['items']
+    return vacancies
