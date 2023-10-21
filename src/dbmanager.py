fd480c8b96e37e98d4c50c83165e8985006bd2cc
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-10-21 20:27:18 +0300

    add def get_companies_and_vacancies_count()

--- src/dbmanager.py
+++ src/dbmanager.py
@@ -10,3 +10,23 @@ class DBManager:
     def __init__(self):
         self.database_name = 'hh_vacancies'
         self.params = config()
+
+    def get_companies_and_vacancies_count(self):
+        """
+        Получает список всех компаний и количество вакансий у каждой компании.
+        """
+        conn = psycopg2.connect(dbname=self.database_name, **self.params)
+
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                SELECT companies.title AS company_name, COUNT(*) AS count_of_vacancies FROM companies
+                INNER JOIN vacancies 
+                USING(company_id) 
+                GROUP BY companies.title
+                """
+            )
+            rows = cur.fetchall()
+            for row in rows:
+                print(row)
+        conn.close()
