5d5746f513c41a6de3354ab386f3f4fbdc54861a
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-10-24 22:17:36 +0300

    add responce.raise_for_status()

--- src/get_data_from_hh.py
+++ src/get_data_from_hh.py
@@ -10,6 +10,7 @@ def get_company(keyword: str) -> dict:
         'text': keyword  # Текст фильтра. Должно быть название компании
     }
     responce = requests.get('https://api.hh.ru/employers', params=params)
+    responce.raise_for_status()
     company = responce.json()
     return company
 
@@ -22,6 +23,7 @@ def get_vacancies_from_company(company: dict) -> list:
         'per_page': COUNT_OF_VACANCIES_IN_PAGE
     }
     responce = requests.get(company['items'][0]['vacancies_url'], params=params)
+    responce.raise_for_status()
     vacancies = responce.json()['items']
     return vacancies
 
@@ -31,6 +33,7 @@ def get_company_data(company: dict) -> dict:
     Возвращает данные о работодателе
     """
     responce = requests.get(company['items'][0]['url'])
+    responce.raise_for_status()
     company_data = responce.json()
     return company_data
 
